8673684db8e8eedde11e6769eae977b84279fa64
vevovevo <111053864+vevovevo@users.noreply.github.com>
2025-10-30 13:46:31 +0100

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import dyadComponentTagger from "@dyad-sh/react-vite-component-tagger";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+export default defineConfig(() => ({
+  base: '/', // Explicitly set the base public path
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [dyadComponentTagger(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: 'dist', // Explicitly set the output directory
+  }
+}));
